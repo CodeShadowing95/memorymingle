b9ab8cf6e2638eea1be4119fb14a15a636e20a49
Frank Patrick Namegni <CodeShadowing95>
2023-04-26 16:38:32 +0200

    Creating models

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { getPosts } from '../controllers/posts.js';
+import { getPosts, createPost } from '../controllers/posts.js';
 
 /* This creates a new instance of an Express router. This router can
 be used to define routes for handling HTTP requests. */
 const router = express.Router();
 
 router.get('/', getPosts);
+router.post('/', createPost);
 
 export default router;
