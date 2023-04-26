26572c21394577d6817bffdce72d617c40a35e64
Frank Patrick Namegni <CodeShadowing95>
2023-04-26 16:27:38 +0200

    Creating controllers

---
+++ server/controllers/posts.js
@@ -0,0 +1,5 @@
+// There we'll create all the handlers for our routes
+
+export const getPosts = (req, res) => {
+  res.send('It works!!!');
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,11 @@
 import express from 'express';
 
+import { getPosts } from '../controllers/posts.js';
+
 /* This creates a new instance of an Express router. This router can
 be used to define routes for handling HTTP requests. */
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('It works!!!');
-});
+router.get('/', getPosts);
 
 export default router;
