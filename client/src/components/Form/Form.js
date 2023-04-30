a6276d19eb880d00abd6642b4e5c226e08250506
Frank Patrick Namegni <CodeShadowing95>
2023-04-30 02:31:26 +0200

    UPDATE: Add some styles for mobile devices

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -67,7 +67,7 @@ const Form = ({ currentId, setCurrentId }) => {
         />
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
         <TextField name="message" variant="outlined" multiline minRows={5} label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
-        <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })} />
+        <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
         <div className={classes.fileInput}>
           <FileBase type="file" multiple={false} onDone={({base64}) => setPostData({ ...postData, image: base64 })} />
         </div>
